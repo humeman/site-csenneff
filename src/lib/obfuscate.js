1a50c351c6c07c987d6eec83fc1b960f86e9fede
humeman <camdenjsf@gmail.com>
2025-07-31 12:59:10 -0500

    Update emails{

--- src/lib/obfuscate.js
+++ src/lib/obfuscate.js
@@ -1,26 +1,29 @@
-const ENCODED = "jhtkluGjzluulmm5jvt"
-const SHIFT = 7
+const ENCODED = "jhtkluGjzluulmm5kl}";
+const SHIFT = 7;
 
 function setEmail(element) {
-    let decoded = "";
+  let decoded = "";
 
-    for (let i = 0; i < ENCODED.length; i++) {
-        decoded += String.fromCharCode(ENCODED.charCodeAt(i) - SHIFT)
-    }
+  for (let i = 0; i < ENCODED.length; i++) {
+    decoded += String.fromCharCode(ENCODED.charCodeAt(i) - SHIFT);
+  }
 
-    element.textContent = decoded;
-    element.href = `mailto:${decoded}`;
+  element.textContent = decoded;
+  element.href = `mailto:${decoded}`;
 }
-
 
 /**
  * An event listener which will detect elements of class "load-email" and
  * calls {@link setEmail} on them.
  */
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
     const elements = document.getElementsByClassName("load-email");
 
     for (let element of elements) {
-        setEmail(element);
+      setEmail(element);
     }
-}, false)
+  },
+  false,
+);
